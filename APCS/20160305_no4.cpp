88c11e744b77154be908a2c4d4f891af8c29b6fb
ANCuber <ansona1131@gmail.com>
2022-03-29 10:57:41 +0800

    ac

---
+++ APCS/20160305_no4.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int index;
+    Node *parent;
+};
+
+int main(){
+    int n; cin>>n;
+    for (int i = 0; i < n-1; ++i) {
+        int a, b;
+        cin>>a>>b;
+
+    }
+}
