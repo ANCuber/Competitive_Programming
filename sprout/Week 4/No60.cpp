53b42f4fa92c982950702fbb74c0e79f27ac9089
ANCuber <ansona1131@gmail.com>
2022-03-29 10:46:12 +0800

    unac

--- sprout/Week 4/No60.cpp
+++ sprout/Week 4/No60.cpp
@@ -3,13 +3,31 @@ using namespace std;
 
 int a, b, c, d;
 int m, s, t;
+//t_s = stop
+//t_m = move
+
+int time(int t_s) {
+    int t_m = t-t_s;
+    m += d*t_s;
+    
+}
+
+int distance(int t_s) {
+
+}
 
 int main() {
     cin>>a>>b>>c>>d;
     cin>>m>>s>>t;
-    int t0 = m/c;
-    m %= c;
-    s -= t0*b;
-    t -= t0;
-    
+    int shortest = 1e9;
+    int farthest = 0;
+    for (int i = 0; i < t; ++i) {// i = stop time
+        int y = time(i);
+        if (y) {
+            if (y < shortest) shortest = y;
+        } else {
+            int x = distance(i);
+            if (x > farthest) farthest = x;
+        }
+    }
 }
