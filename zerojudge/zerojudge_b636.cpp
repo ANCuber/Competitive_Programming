9508b9542c46fcaeceb151e56c3b33c70ce24cd5
ANCuber <ansona1131@gmail.com>
2022-03-31 13:55:21 +0800

    un

--- zerojudge/zerojudge_b636.cpp
+++ zerojudge/zerojudge_b636.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main() {
     unsigned int a, b;
-
+    cin>>a>>b;
+    int ans = 1;
+    while(b) {
+        if (b & 1) {
+            
+        }
+        b >>= 1;
+    }
 }
