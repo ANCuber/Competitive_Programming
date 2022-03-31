f8bdd63219d15338a52a092f1bca79d692a90eae
ANCuber <ansona1131@gmail.com>
2022-03-31 22:03:39 +0800

    ac

--- zerojudge/tempCodeRunnerFile.cpp
+++ zerojudge/tempCodeRunnerFile.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main() {
     unsigned int a, b;
     cin>>a>>b;
-    int ans = 1;
-    while(b) {
+    long long ans = 1;
+    while(b>0) {
         if (b & 1) {
-            
+            ans *= a;
         }
+        a *= a;
         b >>= 1;
+        if (ans > 10007) ans %= 10007;
     }
+    cout<<ans<<'\n';
+    return 0;
 }
