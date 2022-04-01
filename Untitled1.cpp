12c843f13d0d626f40a68db3af9a3761f40a14b6
ANCuber <ansona1131@gmail.com>
2022-04-01 10:40:25 +0800

    lll

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
-#include "lib2243.h"
 using namespace std;
 
-string enter() {
-    return "cran";
+int main() {
+    int q; cin>>q;
+    while(q--) {
+        int x, y, z; 
+        cin>>x>>y>>z;
+        cout<<-1<<'\n';
+    }
+    
+    return 0;
 }
