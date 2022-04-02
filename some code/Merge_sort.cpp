faf1908360754c6488a1c11ded4955a2891d9e65
ANCuber <ansona1131@gmail.com>
2022-04-02 16:39:19 +0800

    sort

--- some code/Merge_sort.cpp
+++ some code/Merge_sort.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void quick_sort(vector <int> &arr, int n) {
+void merge_sort(vector <int> &arr, int n) {
     if (n <= 1) return;
     int leng1 = n/2;
     int leng2 = n-leng1;
     vector <int> arr1(arr.begin(), arr.begin()+leng1);
     vector <int> arr2(arr.begin()+leng1,arr.end());
+    merge_sort(arr1,leng1);
+    merge_sort(arr2,leng2);
+    int temp[n];
+    int len = 0;
+    int p1 = 0, p2 = 0;
+    while(len < n) {
+        if (p2 == leng2 || arr1[p1] <= arr2[p2]) {
+            temp[len++] = arr1[p1++];
+        } else {
+            temp[len++] = arr2[p2++];
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        arr[i] = temp[i];
+    }
+    return;
 }
 
 int main() {
@@ -15,7 +31,7 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin>>arr[i];
     }
-    quick_sort(arr,n);
+    merge_sort(arr,n);
     for (auto i : arr) cout<<i<<' ';
     cout<<'\n';
     return 0;
--- some code/Quick_sort.cpp
+++ some code/Quick_sort.cpp
@@ -1,19 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
+void quick_sort(vector <int> &arr, int n) {
+    if (n <= 1) return;
+    int pivot_index = rand()%n;
+    int pivot = arr[pivot_index];
+    swap(arr[pivot_index],arr[n-1]);
+    int len = 0;
+    for (int i = 0; i < n-1; ++i) {
+        if (arr[i] < pivot) {
+            swap(arr[i],arr[len++]);
+        }
+    }
+    swap(arr[len], arr[n-1]);
+    int len2 = len + 1;
+    for (int i = len2; i < n; ++i) {
+        if (arr[i] == pivot) {
+            swap(arr[i], arr[len2++]);
+        }
+    }
 
-void quick_sort(vector <int> &arr) {
+    vector <int> arr2(arr.begin()+len2, arr.end());
 
+    quick_sort(arr, len);
+    quick_sort(arr2, n-len2);
 }
 
 int main() {
-    cin>>n;
+    int n; cin>>n;
     vector <int> arr(n);
     for (int i = 0; i < n; ++i) {
         cin>>arr[i];
     }
-    quick_sort(arr);
+    quick_sort(arr,n);
     for (auto i : arr) cout<<i<<' ';
     cout<<'\n';
     return 0;
