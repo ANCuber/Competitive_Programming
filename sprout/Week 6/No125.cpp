5e1ee82679a1c20be8ce4c98e55551f9f4f41c63
ANCuber <ansona1131@gmail.com>
2022-04-10 14:45:32 +0800

    ac

--- sprout/Week 6/No125.cpp
+++ sprout/Week 6/No125.cpp
@@ -1,50 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const long long mod = 10000019;
+#define endl '\n'
+#define mod 10000019
 
+int n;
 long long ans = 0;
 
-void solve(vector <int> &arr, int l, int r) {
-    if (l==r) return;
-    if (r-1 == 1) {
-        if (l > r) ans += (long long)(arr[l]%mod + arr[r]%mod);
-        return;
-    }
-    int mid = l+(r-l)/2;
-    vector <int> arr1(mid+1);
-    vector <int> arr2(r-l-mid);
-    for (int i = l; i < mid+1; ++i) {
-        arr1[i] = arr[i];
-    }
-    for (auto i : arr1) cout<<i<<' ';
-    cout<<'\n';
-    for (int i = mid+1; i <= r; ++i) {
-        arr2[i] = arr[i];
-    }
-    for (auto i : arr2) cout<<i<<' ';
-    cout<<'\n';
-    sort(arr1.begin(),arr1.end());
-    sort(arr2.begin(),arr2.end());
-    for (int i = 0; i <= mid; ++i) {
-        int index = mid;
-        while(arr1[i] > arr2[++index]) {
-            ans += (long long)(arr1[i]%mod + arr2[index]%mod);
-        }
-    }
+void solve(vector <long long> &arr, int l, int r) {
+    if (l == r) return;
+    int mid = (l+r)/2;
     solve(arr,l,mid);
     solve(arr,mid+1,r);
+    int index = mid+1;
+    long long presum = 0;
+    for (int i = l; i <= mid; ++i) {
+        while (index <= r && arr[i] > arr[index]) {
+            presum += arr[index++];
+        }
+        //ans += presum%mod;
+        ans += (arr[i]*(index-mid-1) + presum);
+        ans %= mod;
+    } 
+    int temp[r-l+1];
+    int len = 0;
+    int p1 = l, p2 = mid+1;
+    while(len < r-l+1) {
+        if (p1 <= mid && p2 <= r) {
+            if (arr[p1] < arr[p2]) {
+                temp[len++] = arr[p1++];
+            } else {
+                temp[len++] = arr[p2++];
+            }
+        } else {
+            if (p1 == mid+1) {
+                temp[len++] = arr[p2++];
+            } else {
+                temp[len++] = arr[p1++];
+            }
+        }
+    }
+    int id = 0;
+    for (int i = l; i <= r; ++i) {
+        arr[i] = temp[id++];
+    }
+    return;
 }
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cout<<"pass";
-    int n; cin>>n;
-    vector <int> arr(n);
+    cin>>n;
+    vector <long long> arr(n);
     for (int i = 0; i < n; ++i) {
         cin>>arr[i];
     }
     solve(arr,0,n-1);
-    cout<<ans<<'\n';
+    //for (auto i : arr) cout<<i<<' ';
+    //cout<<endl;
+    cout<<ans<<endl;
     return 0;
-}
+} 
