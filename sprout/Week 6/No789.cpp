7df7e28d2413da2e483ab425b9ad7ad625faba5b
ANCuber <ansona1131@gmail.com>
2022-04-11 18:35:38 +0800

    wa

--- sprout/Week 6/No789.cpp
+++ sprout/Week 6/No789.cpp
@@ -3,7 +3,49 @@ using namespace std;
 
 #define endl '\n'
 
+void solve(vector <int> &arr,int n) {
+    if (n == 1) {
+        arr[0] = 1;
+        return;
+    } else if (n == 2) {
+        arr[0] = 1;
+        arr[1] = 2;
+        return;
+    } else if (n == 3) {
+        arr[0] = 3;
+        arr[1] = 1;
+        arr[2] = 2;
+        return;
+    }
+    int subsize = n-(n>>1);
+    vector <int> subarr(subsize,0);
+    solve(subarr,subsize);
+    for (int i = 0; i < subsize; i++) {
+        arr[i] = subarr[i]*2-1;
+    }
+    int id = 0;
+    for (int i = subsize; i < n; i++,id++) {
+        if (subarr[id]*2 > n) {
+            i--;
+            continue;
+        }
+        arr[i] = subarr[id]*2;
+
+    }
+}
+
 int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n; cin>>n;
-    
+    vector <int> arr(n,0);
+    solve(arr,n);
+    vector <int> x(n,0);
+    for (int i = 0; i < n; ++i) {
+        if (i) cout<<' ';
+        cout<<arr[i];
+        x[arr[i]]++;
+        if (x[arr[i]] > 1) cout<<"fail"<<endl;
+    }
+    cout<<endl;
+    return 0;
 }
