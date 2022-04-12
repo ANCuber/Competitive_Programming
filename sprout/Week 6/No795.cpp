a07d551221495d3354345c6f58324b67b54c5a6d
ANCuber <ansona1131@gmail.com>
2022-04-12 22:47:15 +0800

    4/12

--- sprout/Week 6/No795.cpp
+++ sprout/Week 6/No795.cpp
@@ -6,18 +6,52 @@ using namespace std;
 struct Dot {
     long long x;
     long long y;
-    bool operator<(Dot b) {
-        if (x == b.x) return y < b.y;
-        return x < b.x;
-    }
 };
 
-long long min_d = 1e12;
+bool cmp_x (Dot a, Dot b){
+    return a.x < b.x;
+}
+bool cmp_y (Dot a, Dot b){
+    return a.y < b.y;
+}
 
-void solve(int l, int r, vector <Dot> &dots) {
-    if (l == r) return;
+long long solve(int l, int r, vector <Dot> &dots) {
+    if (r - l == 1) {
+        long long xd = dots[r].x-dots[l].x;
+        long long yd = dots[r].y-dots[l].y;
+        return xd*xd+yd*yd;
+    } else if (r - l == 2) {
+        long long d = LONG_LONG_MAX;
+        for (int i = l; i <= l+1; ++i) {
+            for (int j = i+1; j <= l+2; ++j) {
+                long long xd = dots[i].x-dots[j].x;
+                long long yd = dots[i].y-dots[j].y;
+                if (xd*xd+yd*yd < d) d = xd*xd+yd*yd;
+            }
+        }
+        return d;
+    }
     int mid = (l+r)>>1;
-
+    long long min_c = min(solve(l,mid,dots),solve(mid+1,r,dots));
+    long long min_dis = LONG_LONG_MAX;
+    long long l_fund = dots[mid].x, l_horz = dots[mid].y;
+    long long r_fund = dots[mid+1].x, r_horz = dots[mid+1].y;
+    if (r_fund - l_fund >= min_c) return min_c;
+    int r_range = r_fund-min_c;//***|
+    int l_range = l_fund+min_c;// |***
+    for (int i = l; i <= r; ++i) {
+        if (dots[i].x >= r_range && dots[i].x <= r_fund) {
+            long long xd = dots[i].x - dots[r].x;
+            long long yd = dots[i].y - dots[r].y;
+            min_dis = min(min_dis,xd*xd+yd*yd);
+        }
+        if (dots[i].x <= l_range && dots[i].x >= l_fund) {
+            long long xd = dots[i].x - dots[l].x;
+            long long yd = dots[i].y - dots[l].y;
+            min_dis = min(min_dis,xd*xd+yd*yd);
+        }
+    }
+    return min(min_c,min_dis);
 }
 
 int main() {
@@ -27,8 +61,7 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin>>dots[i].x>>dots[i].y;
     }
-    sort(dots.begin(),dots.end());
-    solve(0,n,dots);
-    cout<<min_d<<endl;
+    sort(dots.begin(),dots.end(),cmp_x);
+    cout<<solve(0,n-1,dots)<<endl;
     return 0;
 }
