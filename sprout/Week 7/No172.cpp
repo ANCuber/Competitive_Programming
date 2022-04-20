b1ff39f341b61d7777075316fcd412537feb5972
ANCuber <ansona1131@gmail.com>
2022-04-20 22:56:50 +0800

    4/20

---
+++ sprout/Week 7/No172.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define mod 100z0007
+
+int main() {
+    int t; cin>>t; 
+    while(t--) {
+        int n, m;
+        cin>>n>>m;
+        vector <int> amount(n);
+        for (int i = 0; i < n; ++i) cin>>amount[i];
+        
+    }
+}
