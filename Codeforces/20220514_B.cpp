614679b49096d4632af6e96c4d481d35a6c11fc3
ANCuber <ansona1131@gmail.com>
2022-05-14 22:29:15 +0800

    5/14

--- Codeforces/20220514_B.cpp
+++ Codeforces/20220514_B.cpp
@@ -5,35 +5,35 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, q; cin>>n>>q;
-    vector <int> arr(n);
+    long long n, q; cin>>n>>q;
+    vector <long long> arr(n);
     long long sum = 0;
     for (int i = 0; i < n; ++i) {
         cin>>arr[i];
         sum += arr[i];
     }
-    bitset <200005> ctrl(0);
-    int num;
+    bitset <200005> ctrl(false);
+    long long num;
     for (int i = 0; i < q; ++i) {
         int t; cin>>t;
         if (t == 1) {
-            int id, x; cin>>id>>x;
+            long long id, x; cin>>id>>x;
             if (ctrl[id-1]) {
                 sum -= num;
                 sum += x;
                 arr[id-1] = x;
-                ctrl[id-1] = false;
             } else {
                 sum -= arr[id-1];
                 arr[id-1] = x;
                 sum += x;
             }
+            ctrl[id-1] = false;
         } else if (t == 2){
-            int x; cin>>x;
-            sum = (long long)(n*x);
+            cin>>num;
+            sum = n*num;
             ctrl.set();
-            num = x;
         }
         cout<<sum<<endl;
     }
+    return 0;
 }
--- sprout/Week 9/No136.cpp
+++ sprout/Week 9/No136.cpp
@@ -1,21 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//meet in middle algorithm
+
 #define endl '\n'
 
-void Report(int id) {
+void Report(long long id) {
     cout<<id<<endl;
 }
 
-void solve(int n, long long k, long long a[]) {
-    
+
+void solve(long long n, long long k, long long a[]) {
+    pair <long long,long long> store[1LL<<(n/2)];
+    for (long long i = 0; i < (1LL<<(n/2)); ++i) {
+        long long sum = 0;
+        for (long long j = 0; j < n/2; ++j) {
+            sum += a[j]*((i>>j)&1);
+        }
+        store[i] = {sum,i};
+    }
+    sort(store,store+(1LL<<(n/2)));
+    for (long long i = 0; i < (1LL<<(n-n/2)); ++i) {
+        long long sum = 0;
+        for (long long j = 0; j < n-n/2; ++j) {
+            sum += a[j+n/2]*((i>>j)&1);
+        }
+
+        long long l = 0;
+        long long r = 1LL<<(n/2);
+        long long mid = l+(r-l)/2;
+        while (r-l > 1) {
+            if (store[mid].first > k-sum) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+            mid = l+(r-l)/2;
+        }
+        if (store[l].first == k-sum) {
+            long long all = store[l].second + ((1LL*i)<<(n/2));
+            for (long long i = 0; i < n; ++i) {
+                if ((all>>i)&1) Report(i+1);
+            }
+            Report(-1);
+            return;
+        }
+    }
+    Report(-1);
+    return;
 }
 
 int main() {
-    int n; cin>>n;
+    long long n; cin>>n;
     long long k; cin>>k;
     long long a[n];
-    for (int i = 0; i < n; ++i) {
+    for (long long i = 0; i < n; ++i) {
         cin>>a[i];
     }
     solve(n,k,a);
