0f50b66ba8c9fe6ce45238bb65230ab982729291
ANCuber <ansona1131@gmail.com>
2022-05-16 11:55:15 +0800

    5/16

--- sprout/Week 10/No159.cpp
+++ sprout/Week 10/No159.cpp
@@ -10,7 +10,8 @@ int main() {
         int n, m;
         cin>>n>>m;
         int coin[n][2];//c,k
-        vector <int> item;
+        vector <int> item(1005);
+        int sum = 0;
         for (int i = 0; i < n; ++i) {
             cin>>coin[i][0]>>coin[i][1];
             int v = coin[i][0];
@@ -19,24 +20,22 @@ int main() {
             while (k) {
                 if (base <= k) k -= base;
                 else {
-                    item.emplace_back(k*v);
+                    item[sum++] = k*v;
                     break;
                 }
-                item.emplace_back(v*base);
+                item[sum++] = v*base;
                 base<<=1;
             }
         }
-        for (auto i : item) cout<<i<<' ';
-        cout<<endl;
-        int sum = item.size();
         int dp[2][m+1];
         for (int i = 0; i < sum; ++i) {
-            for (int j = 0; j <= m; ++j) {
+            for (int j = 0; j <= m; j++) {
                 if (i == 0) {
                     if (j == item[0]) dp[0][j] = 1;
                     else dp[0][j] = 0;
                 } else {
-                    if (j >= item[i]) dp[i%2][j] = max(dp[(i+1)%2][j],dp[(i+1)%2][j-item[i]]);
+                    if (j == item[i]) dp[i%2][j] = 1;
+                    else if (j > item[i]) dp[i%2][j] = max(dp[(i+1)%2][j],dp[(i+1)%2][j-item[i]]);
                     else dp[i%2][j] = dp[(i+1)%2][j];
                 }
             }
