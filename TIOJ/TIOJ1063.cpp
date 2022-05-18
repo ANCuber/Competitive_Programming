47406f1026f063c224c06c458bbe39af86e40680
ANCuber <ansona1131@gmail.com>
2022-05-18 23:02:43 +0800

    5/18

--- TIOJ/TIOJ1063.cpp
+++ TIOJ/TIOJ1063.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 #define ll long long
@@ -8,64 +9,58 @@ int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int m, n; cin>>m>>n;
     int graph[m][n];
-    vector <int> height(n);
+    vector <int> height(n,0);
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
             cin>>graph[i][j];
             if (!i && graph[i][j]) height[j] = 1;
         }
-    }
-    int ans = 0;
+    }//input
+    ll ans = 0;
+    //cout<<endl;
     for (int i = 0; i < m; ++i) {
         vector <int> left;
         vector <int> right;
-        vector <int> curans(n,0);
+        vector <ll> curans(n,0);
         for (int j = 0; j < n; ++j) {
-            if (!height[j]) {
-                left.clear();
-                continue;
-            }
-            while(!left.empty()){
-                if (height[left.back()] > height[j]) {
+            while(!left.empty()) {
+                if (height[j] <= height[left.back()]) {
                     left.pop_back();
                 } else {
                     break;
                 }
             }
             if (left.empty()) {
-                curans[j] += height[j];
-                continue;
+                curans[j] += (j+1)*height[j];
+            } else {
+                curans[j] += (j-left.back())*height[j];
             }
-            int id = left.size()-1;
-            while(id >= 0 && height[left[id]] == height[j]) {
-                id--;
-            }
-            curans[j] += (j-left[id]+1)*height[j];
             left.push_back(j);
         }
         for (int j = n-1; j >= 0; --j) {
-            if (!height[j]) {
-                right.clear();
-                continue;
-            }
-            while(!right.empty()){
-                if (height[right.back()] > height[j]) {
+            while (!right.empty()) {
+                if (height[j] <= height[right.back()]) {
                     right.pop_back();
                 } else {
                     break;
                 }
             }
             if (right.empty()) {
-                continue;
+                curans[j] += (n-1-j)*height[j];
+            } else {
+                curans[j] += (right.back()-j-1)*height[j];
             }
-            int id = right.size()-1;
-            while(id >= 0 && height[right[id]] == height[j]) {
-                id--;
-            }
-            curans[j] += (right[id]-j)*height[j];
-            left.push_back(j);
+            right.push_back(j);
+            ans = max(ans,curans[j]);
+            //cout<<curans[j]<<' ';
         }
-        ans = max(*max_element(curans.begin(),curans.end()),ans);
+        if (i != m-1) {
+            for (int j = 0; j < n; ++j) {
+                if (graph[i+1][j]) height[j]++;
+                else height[j] = 0;
+            }
+        }
+        //cout<<endl;
     }
     cout<<ans<<endl;
     return 0;
