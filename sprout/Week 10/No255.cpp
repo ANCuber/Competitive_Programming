5f2d334574425d5d9b93877e08ab1b2ef86d1529
ANCuber <ansona1131@gmail.com>
2022-05-19 13:20:10 +0800

    5/19

--- sprout/Week 10/No255.cpp
+++ sprout/Week 10/No255.cpp
@@ -4,19 +4,37 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
+int n;
+
 struct Dot{
     int x, y;
 };
 
+tuple<ll,ll,ll> coeff(int x1, int y1, int x2, int y2) {
+    ll del = 1LL*x1*x2*(x1-x2);
+    ll del_a;
+    ll del_b;
+}
+
+int solve(vector <Dot> &p, vector <int> &dp,int cur, int counter) {
+    if(dp[cur]) return dp[cur];
+
+}
+
 int main() {
     int t; cin>>t;
     while(t--) {
-        int n; cin>>n;
+        cin>>n;
+        if (n == 1) {
+            cout<<1<<endl;
+            continue;
+        }
+        vector <int> dp(1<<n,0);
         vector <Dot> pig(n);
         for (int i = 0; i < n; ++i) cin>>pig[i].x>>pig[i].y;
-
-
+        cout<<solve(pig,dp,1<<n-1,0)<<endl;
     }
+    return 0;
 }
 
 /*
