8fef828d01225f6955e95af16af7d9bcc455db05
ANCuber <ansona1131@gmail.com>
2022-05-22 11:31:16 +0800

    5/22

--- TIOJ/TIOJ1015.cpp
+++ TIOJ/TIOJ1015.cpp
@@ -5,7 +5,9 @@ using namespace std;
 #define endl '\n'
 
 int main() {
-    int n, k;
-    vector <ll> score(n+1);
-    for (int i = 1; i <= n; ++i) cin>>score[i];
+    ll m, n;
+    
+    while(cin>>m>>n) {
+
+    }
 }
--- TIOJ/TIOJ1064.cpp
+++ TIOJ/TIOJ1064.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define endl '\n'
+
 int main() {
-    string a, b;
-    cin>>a>>b;
-    
+    string A, B;
+    cin>>A>>B;
+    ll len_a = A.size(), len_b = B.size();
+    vector <int> a(len_a);
+    vector <int> b(len_b);
+    for (int i = 0; i < len_a; ++i) a[i] = A[i]-'0';
+    for (int i = 0; i < len_b; ++i) b[i] = b[i]-'0';
+    vector <int> ans;
+    for (int i = 0; i < len_b; ++i) {
+        
+    }
 }
