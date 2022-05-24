2b5d878dde0e047a193052939ed63375aa2eaa04
ANCuber <ansona1131@gmail.com>
2022-05-24 09:52:06 +0800

    5/24

---
+++ sprout/Week 11/No793.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main() {
+    int n, k, q;
+    cin>>n>>k>>q;
+    vector <int> arr(n);
+    for (int i = 0; i < n; ++i) cin>>arr[i];
+    while(q--) {
+        int l, r; cin>>l>>r;
+        
+    }
+    return 0;
+}
