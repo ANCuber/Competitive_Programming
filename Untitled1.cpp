3aa51f9d2948a8301ea37d017337e0aac9f9f4d1
ANCuber <ansona1131@gmail.com>
2022-05-25 14:27:56 +0800

    5/25

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,32 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
 using namespace std;
 
-const int N = 6;
-
-struct Matrix{
-    long long a[N][N];
-    int n;
-    void init(int size) {
-        n = size;
-        memset(a,0,sizeof(a));
-    }
-    void I() {
-        memset(a,0,sizeof(a));
-        for (int i = 0; i < n; ++i) {
-            a[i][i] = 1;
-        }
-    }
-};
-
 int main() {
-    Matrix a;
     int n; cin>>n;
-    a.init(n);
-    for (int i = 0; i < a.n; ++i) {
-        for (int j = 0; j < a.n; ++j) {
-            cout<<a.a[i][j]<<' ';
-        }
-        cout<<endl;
+    multiset <int> lis;
+    for (int i = 0; i < n; ++i) {
+        int a; cin>>a;
+        lis.insert(a);
+        auto iter = lis.upper_bound(a);
+        if (iter != lis.end()) lis.erase(iter);
     }
+    cout<< lis.size() << endl;
     return 0;
 }
