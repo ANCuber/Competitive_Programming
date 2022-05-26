a9f4c96b6f8744d0ca5c64691b806e4efe3c259e
ANCuber <ansona1131@gmail.com>
2022-05-26 15:45:44 +0800

    5/26

--- TIOJ/TIOJ1009.cpp
+++ TIOJ/TIOJ1009.cpp
@@ -2,25 +2,20 @@
 using namespace std;
 
 #define endl '\n'
-int number[8] = {100000,10000,0,1000,100,0,10,1};
+int x[3] = {3600,60,1};
 
 int main() {
     string a , b;
     cin>>a>>b;
     int at = 0, bt = 0;
-    int j = 100000;
-    for (int i = 0; i < 8; ++i) {
-        if (i == 2 || i == 5) continue;
-        at += (a[i]-'0')*j;
-        bt += (b[i]-'0')*j;
-        j /= 10;
+    for (int i = 0; i < 8; i+=3) {
+        at += ((a[i]-'0')*10+(a[i+1]-'0'))*x[i/3];
+        bt += ((b[i]-'0')*10+(a[i+1]-'0'))*x[i/3];
     }
-    if (at > bt) bt += 240000;
+    if (at > bt) bt += 3600*24;
     int ans = bt-at;
-    for (int i = 0; i < 8; ++i) {
-        if (i == 2 || i == 5) cout<<':';
-        else cout<<(ans/number[i])%10;
+    for (int i = 0; i < 3; ++i) {
+        
     }
-    cout<<endl;
     return 0;
 }
