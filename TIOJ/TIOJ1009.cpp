02660ce357a26994691234fa2b2c70f954ed57e5
ANCuber <ansona1131@gmail.com>
2022-05-26 16:05:53 +0800

    5/26

--- TIOJ/TIOJ1009.cpp
+++ TIOJ/TIOJ1009.cpp
@@ -11,13 +11,14 @@ int main() {
     int at = 0, bt = 0;
     for (int i = 0; i < 8; i+=3) {
         at += ((a[i]-'0')*10+(a[i+1]-'0'))*x[i/3];
-        bt += ((b[i]-'0')*10+(a[i+1]-'0'))*x[i/3];
+        bt += ((b[i]-'0')*10+(b[i+1]-'0'))*x[i/3];
     }
+    //cout<<at<<' '<<bt<<endl;
     if (at > bt) bt += 3600*24;
     int ans = bt-at;
     for (int i = 0; i < 3; ++i) {
         int s = ans/x[i];
-        else if (s < 10) cout<<0<<s;
+        if (s < 10) cout<<0<<s;
         else cout<<s;
         if (i != 2)cout<<':';
         ans %= x[i];
