a26cc406a387067fa4c72dd8e2929ca763e68d91
ANCuber <ansona1131@gmail.com>
2022-06-07 17:51:29 +0800

    6/7

--- zerojudge/zerojudge_h028.cpp
+++ zerojudge/zerojudge_h028.cpp
@@ -5,5 +5,11 @@ using namespace std;
 #define endl '\n'
 
 int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, l; cin>>n>>l;
+    vector <pair<int,int> > tree(n);
+    for (int i = 0; i < n; ++i) cin>>tree[i].first;
+    for (int i = 0; i < n; ++i) cin>>tree[i].second;
     
+
 }
