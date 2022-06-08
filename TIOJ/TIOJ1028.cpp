6972e2333d0285c63e73b09ccc96ed9148fa6d6b
ANCuber <ansona1131@gmail.com>
2022-06-08 16:17:03 +0800

    6/8

--- TIOJ/TIOJ1028.cpp
+++ TIOJ/TIOJ1028.cpp
@@ -5,5 +5,17 @@ using namespace std;
 #define ll long long
 
 int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, m; cin>>n>>m;
+    vector<vector<int> > graph(n,vector<int>(n,1e9));
+    for (int i = 0; i < m; ++i) {
+        int a, b; cin>>a>>b;
+        int d; cin>>d;
+        graph[a][b] = d;
+        graph[b][a] = d;
+    }
+    int k; cin>>k;
+    vector<int> des(k);
+    for (int i = 0; i < k;++i) cin>>des[i];
     
 }
---
+++ zerojudge/zerojudge_e288.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+
+int code(char c) {
+    if (c-'a' >= 0) return c-'a';
+    return c-'A';
+}
+
+int main() {
+    int m, n; cin>>m>>n;
+    for (int i = 0; i < n; ++i) {
+
+    }
+}
