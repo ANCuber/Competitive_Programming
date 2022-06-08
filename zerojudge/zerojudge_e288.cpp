a44fbd69ae0624f3562240e1624c2e3a703303b8
ANCuber <ansona1131@gmail.com>
2022-06-08 23:13:31 +0800

    6/8

--- zerojudge/zerojudge_e288.cpp
+++ zerojudge/zerojudge_e288.cpp
@@ -4,14 +4,30 @@ using namespace std;
 #define endl '\n'
 #define ll long long
 
-int code(char c) {
-    if (c-'a' >= 0) return c-'a';
+ll code(char c) {
+    if (c-'a' >= 0) return c-'a'+26;
     return c-'A';
-}
+}//A,B,C...Z,a,b,c...z
 
 int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int m, n; cin>>m>>n;
+    unordered_multiset<ll> mp;
     for (int i = 0; i < n; ++i) {
-
+        ll sc = 0;
+        string a; cin>>a;
+        for (int j = 0; j < a.size(); ++j) {
+            sc = sc|(1LL<<(code(a[j])));
+        }
+        mp.insert(sc);
     }
+    ll comp = (1LL<<m)-1;
+    ll ans = 0;
+    for (auto i : mp) {
+        ll opp = comp^i;
+        ans += mp.count(opp);
+    }
+    ans >>= 1;
+    cout<<ans<<endl;
+    return 0;
 }
--- zerojudge/zerojudge_h028.cpp
+++ zerojudge/zerojudge_h028.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 #define ll long long
-#define endl '\n'
+#define endl '\n';
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, l; cin>>n>>l;
-    vector <pair<int,int> > tree(n);
-    for (int i = 0; i < n; ++i) cin>>tree[i].first;
-    for (int i = 0; i < n; ++i) cin>>tree[i].second;
-    
+    vector <int> c(n+2);
+    vector <int> h(n+2);
+    c[0] = 0;
+    h[0] = 2e9;
+    for (int i = 1; i <= n; ++i) cin>>c[i];
+    for (int i = 1; i <= n; ++i) cin>>h[i];
+    c[n+1] = l;
+    h[n+1] = 2e9;
+
 
 }
