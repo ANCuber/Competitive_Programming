51bff50d76907b2d53089f1516949559677e4b6f
ANCuber <ansona1131@gmail.com>
2022-06-08 11:57:45 +0800

    6/8

--- zerojudge/zerojudge_f314.cpp
+++ zerojudge/zerojudge_f314.cpp
@@ -5,13 +5,34 @@ using namespace std;
 #define ll long long
 
 int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int m, n; cin>>m>>n;
-    int graph[m][n];
-    for (int i = 0;i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin>>graph[i][j];
+    vector <int> arr(n+1);
+    vector <int> l(n+1);
+    vector <int> r(n+1);
+    vector <vector<int> > dp(m+1,vector<int>(n+1,0));
+    for (int i = 1;i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cin>>arr[j];
+            if (j == 1) {
+                l[j] = dp[i-1][j] + arr[j];
+            } else {
+                l[j] = max(dp[i-1][j],l[j-1]) + arr[j];
+            }  
+        }
+        for (int j = n; j > 0; --j) {
+            if (j == n) {
+                r[j] = dp[i-1][j] + arr[j];
+            } else {
+                r[j] = max(dp[i-1][j],r[j+1]) + arr[j];
+            }
+            dp[i][j] = max(l[j],r[j]);
         }
     }
-    
+    int ans = -1e9;
+    for (int i = 1; i <= n; ++i) {
+        ans = max(ans,dp[m][i]);
+    }
+    cout<<ans<<endl;
     return 0;
 }
--- zerojudge/zerojudge_f315_segtree.cpp
+++ zerojudge/zerojudge_f315_segtree.cpp
@@ -4,15 +4,27 @@ using namespace std;
 #define endl '\n'
 #define ll long long
 
+vector <int> st;
+vector <int> arr;
+vector <vector<int> > id;
+
+void query(int node, int num, int ql, int qr, int l, int r) {
+    
+}
+
 int main() {
     int n; cin>>n;
-    vector <int> arr((n<<1)+1);
+    st.resize(n<<3,0);
+    arr.resize((n<<1)+1);
+    id.resize(n+1);
     for (int i = 1; i <= (n<<1); ++i) {
         cin>>arr[i];
+        id[arr[i]].push_back(i);
     }
     ll ans = 0;
     for (int i = 1; i <= n; ++i) {
 
     }
+    cout<<ans<<endl;
     return 0;
 }
