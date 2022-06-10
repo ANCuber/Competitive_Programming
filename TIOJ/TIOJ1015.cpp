217bec2cb76a01cd2bbcf10877aa714bcfc79e74
ANCuber <ansona1131@gmail.com>
2022-06-10 11:41:24 +0800

    6/10

--- TIOJ/TIOJ1015.cpp
+++ TIOJ/TIOJ1015.cpp
@@ -5,9 +5,14 @@ using namespace std;
 #define endl '\n'
 
 int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     ll m, n;
     
     while(cin>>m>>n) {
-
+        if (!m && !n) break;
+        if (m > n) swap(n,m);
+        ll ans = m*(m+1)*(3*n-m+1)/6;
+        cout<<ans<<endl;
     }
+    return 0;
 }
