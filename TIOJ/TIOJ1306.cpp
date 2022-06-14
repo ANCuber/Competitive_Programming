e4bde7f316e18852a66435c18b1f8b386a3090cf
ANCuber <ansona1131@gmail.com>
2022-06-14 16:46:52 +0800

    6/14

---
+++ TIOJ/TIOJ1306.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+void failure(vector<int> &b, string p) {
+
+}
+
+int KMP() {
+    
+}
+
+int main() {
+    int t; cin>>t;
+    while(t--) {
+        string t; cin>>t;
+        int q; cin>>q;
+        while(q--) {
+            string p; cin>>p;
+            vector <int> border(p.size());
+            border[0] = -1;
+            failure(border,p);
+
+        }       
+    }
+    return;
+}
--- sprout/Week 14/No257.cpp
+++ sprout/Week 14/No257.cpp
@@ -5,29 +5,49 @@ using namespace std;
 #define endl '\n'
 
 struct Nd{
-    int lat = 0;
-    int val;
-    int ori;
+    int c1 = 0, c2 = 0, c3 = 0;
+    int lazt = 0;
     bool reset = 0;
 };
 
 vector <Nd> seg;
 
-int init(int l, int r, int node) {
-    if (l == r) {
-        seg[node].val = seg[node].ori = 1;
-        return 1;
-    }
-    int mid = (l+r)/2;
-    return seg[node].val = seg[node].ori = init(l,mid,node<<1)+init(mid+1,r,(node<<1)|1);
+inline void Merge(Nd &ret,const Nd &a,const Nd &b) {
+    ret.c1 = a.c1+b.c1;
+    ret.c2 = a.c2+b.c2;
+    ret.c3 = a.c3+b.c3;
 }
 
-int turn(int ql, int qr, int l, int r, int node) {
-    
+void init(int l, int r, int node) {
+    if (l == r) {
+        seg[node].c1 = 1;
+        return;
+    }
+    int mid = (l+r)/2;
+    init(l,mid,node<<1);
+    init(mid+1,r,(node<<1)|1);
+    seg[node].c1 = seg[node<<1].c1+seg[(node<<1)|1].c1;
+    return;
+}
+
+Nd turn(int ql, int qr, int l, int r, int node) {
+    if (ql <= l && r <= qr) {
+        swap(seg[node].c1,seg[node].c2);
+        swap(seg[node].c1,seg[node].c3);
+        seg[node].lazt++;
+        return seg[node];
+    }
+    int mid = (l+r)>>1;
+    if (qr <= mid) return turn(ql,qr,l,mid,node<<1);
+    if (ql > mid) return turn(ql,qr,mid+1,r,(node<<1)|1);
+    Merge(seg[node],turn(ql,qr,l,mid,node<<1),turn(ql,qr,mid+1,r,(node<<1)|1));
+    return seg[node];
 }
 
 int reset(int ql, int qr, int l, int r, int node) {
-
+    if (ql <= l && r <= qr) {
+        
+    }
 }
 
 int query(int ql, int qr, int l, int r, int node) {
@@ -36,7 +56,7 @@ int query(int ql, int qr, int l, int r, int node) {
 
 int main() {
     int n, m; cin>>n>>m;
-    seg.resize(n<<1);
+    seg.resize(n<<2);
     init(1,n,1);
     while(m--) {
         string cmd; cin>>cmd;
