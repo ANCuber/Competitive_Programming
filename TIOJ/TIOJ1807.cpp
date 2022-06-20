72e80514afcd8445410e300c29890f4fed9de3b6
ANCuber <ansona1131@gmail.com>
2022-06-20 19:01:52 +0800

    6/20

---
+++ TIOJ/TIOJ1807.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+
+int m, n;
+bool ctrl = 0;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cin>>m>>n;
+    
+}
