be971ea9e0ac06905980c4a92dad79e785da1be7
ANCuber <ansona1131@gmail.com>
2022-06-25 17:07:54 +0800

    6/25

--- sprout/Test2/pC.cpp
+++ sprout/Test2/pC.cpp
@@ -7,13 +7,12 @@ using namespace std;
 int main() {
     int n, m, k;
     cin>>n>>m>>k;
-    vector <vector<ll> > g(n+1,vector<ll>(n+1,1e12));
     while(m--) {
         int u, v;
         ll l;
         cin>>u>>v>>l;
-        g[u][v] = g[v][u] = l;
+        
     }
-    
+
     return 0;
 }
