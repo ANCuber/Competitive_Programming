1adfaf83e39a737f6324f859732e36d074310b4b
ANCuber <ansona1131@gmail.com>
2022-06-27 23:10:11 +0800

    6/27

--- TIOJ/1703.cpp
+++ TIOJ/1703.cpp
@@ -4,18 +4,17 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
-ll g(ll x) {
-    if (!x%3) return 1+g(x/3);
-    else return 0;
-}
-
-ll f(ll x) {
-    if (x <= 0) return 0;
-    return g(x)+f(x-1);
-}
-
 int main() {
-    ll n; cin>>n;
-    cout<<f(n)<<endl;
+    ll n;
+    ll ans = 0;
+    while(cin>>n) {
+        ans = 0;
+        ll tmp = 3;
+        while(n >= tmp) {
+            ans += n/tmp;
+            tmp *= 3;
+        }
+        cout<<ans<<endl;
+    }
     return 0;
 }
