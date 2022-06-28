df4afc163632d4cfba1d2e85e5657a1c8b6cc3dc
ANCuber <ansona1131@gmail.com>
2022-06-28 13:22:39 +0800

    6/28

---
+++ TIOJ/1809.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int,int>
+#define endl '\n'
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, m, q;
+    cin>>n>>m>>q;
+    int a, b;
+    while(m--) {
+        cin>>a>>b;
+        
+    }
+    return 0;
+}
