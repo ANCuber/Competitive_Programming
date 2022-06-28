6f835449a3cffdbd2271fd1f240d2864a9b00ea2
ANCuber <ansona1131@gmail.com>
2022-06-28 21:07:58 +0800

    6/28

--- TIOJ/1809.cpp
+++ TIOJ/1809.cpp
@@ -8,10 +8,16 @@ int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, m, q;
     cin>>n>>m>>q;
+    set < pii > st;
     int a, b;
     while(m--) {
         cin>>a>>b;
-        
+        st.insert({min(a,b),max(a,b)});
+    }
+    while(q--) {
+        cin>>a>>b;
+        if (st.find({min(a,b),max(a,b)}) != st.end()) cout<<"yes\n";
+        else cout<<"no\n";
     }
     return 0;
 }
