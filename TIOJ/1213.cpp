12b3715092c2ccb9e784100f9ea868ae077475bd
ANCuber <ansona1131@gmail.com>
2022-06-30 18:23:40 +0800

    6/30" F
    FAD
    E12

---
+++ TIOJ/1213.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    while(cin>>n && n) {
+        
+    }
+}
