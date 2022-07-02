d758c6acf11e4a2a161710f5f484b5d195cfafdc
ANCuber <ansona1131@gmail.com>
2022-07-02 17:52:46 +0800

    7/2

--- TIOJ/1336.cpp
+++ TIOJ/1336.cpp
@@ -5,8 +5,18 @@ using namespace std;
 #define endl '\n'
 #define pii pair<int,int>
 
-void dfs(int x, int y, vector<vector<int> > &gr, int tp) {
-    
+int dx[8] = {0,0,1,-1,-1,1,1,-1};
+int dy[8] = {1,-1,0,0,1,-1,1,-1};
+
+int ans[3] = {0,0,0};
+
+void dfs(int x, int y, vector<vector<int> > &g, int ty) {
+    g[x][y] = 0;
+    for (int i = 0; i < 8; ++i) {
+        if (g[x+dx[i]][y+dy[i]] == ty) {
+            dfs(x+dx[i],y+dy[i],g,ty);
+        }
+    }
 }
 
 int main() {
@@ -24,12 +34,14 @@ int main() {
             }
         }
     }
-    sp = g;
-    int green = 0, space = 0;
     for (int i = 1; i <= h; ++i) {
         for (int j = 1; j <= w; ++j) {
-            
+            if (g[i][j]) {
+                ans[g[i][j]]++;
+                dfs(i,j,g,g[i][j]);
+            }
         }
     }
+    cout<<ans[2]<<' '<<ans[1]<<endl;
     return 0;
 }
