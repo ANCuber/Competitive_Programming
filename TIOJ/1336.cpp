2616cdf4726ece2edd5b87b9e30f308034c4711b
ANCuber <ansona1131@gmail.com>
2022-07-02 14:09:09 +0800

    7/2

---
+++ TIOJ/1336.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int w, h; cin>>w>>h;
+    vector<vector<int> > g(h+2,vector<char>(w+2,0)), sp;
+    char a;
+    for (int i = 1; i <= h; ++i) {
+        for (int j = 1; j <= w; ++j) {
+            cin>>a;
+            if (a == '-') {
+                g[i][j] = 1;
+            } else if (a == 'G') {
+                g[i][j] = 2;
+            }
+        }
+    }
+    sp = g;
+    int green = 0, space = 0;
+    for (int i = 1; i <= h; ++i) {
+        for (int j = 1; j <= w; ++j) {
+            
+        }
+    }
+}
---
+++ TIOJ/1801.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int m; cin>>m;
+    int n, k;
+    for (int k = 1; k <= m; ++k) {
+        cin>>n>>k;
+        n--;
+        while(n--) {
+            
+        }
+    }
+}
