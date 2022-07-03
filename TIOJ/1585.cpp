97ea32dab20eee3847d2d0f0717fd0732115b558
ANCuber <ansona1131@gmail.com>
2022-07-03 17:51:20 +0800

    7/3

---
+++ TIOJ/1585.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+
+map <char,int> mp;
+
+struct Cr{
+    char x;
+
+};
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n; cin>>n;
+    vector <Cr> arr(n);
+    string ipt; cin>>ipt;
+    for (int i = 0; i < n; ++i)
+}
