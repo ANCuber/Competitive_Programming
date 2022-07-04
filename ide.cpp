4a72806dd3c6967eaa03cba2a23f01b6c7fa88e3
ANCuber <ansona1131@gmail.com>
2022-07-04 23:21:02 +0800

    7/4

--- ide.cpp
+++ ide.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
+
+for (int = 1; i < n; ++i) {
+    for updated Edges:
+        dis[v] = min(dis[v],dis[u]+w[u][v]);
+}
 
 int main() {
     int a, b;
