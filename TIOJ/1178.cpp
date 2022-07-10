06dda1d226bba3c0942b1dd93d511b89a2b93af2
ANCuber <ansona1131@gmail.com>
2022-07-10 16:46:04 +0800

    7/9

--- TIOJ/1178.cpp
+++ TIOJ/1178.cpp
@@ -5,8 +5,17 @@ using namespace std;
 #define endl '\n'
 #define pii pair<int,int>
 
+struct Point{
+    int x;
+    int y;
+}
+
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, st; cin>>n>>st;
+    int n; cin>>n;
+    vector <Point> p(n);
+    for (int i = 0; i < n; ++i) {
+        cin>>p[i].x>>p[i].y;
+    }
     return 0;
 }
--- TIOJ/1587.cpp
+++ TIOJ/1587.cpp
@@ -7,6 +7,13 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, st; cin>>n>>st;
+    string n; cin>>n;
+    for (int i = 0; i < n.size(); ++i) {
+        cout<<n[i];
+    }
+    for (int i = n.size()-2; i >= 0; --i) {
+        cout<<n[i];
+    }
+    cout<<endl;
     return 0;
 }
