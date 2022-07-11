d041cc7929de4648a5cdc20d905801dec1daae9e
ANCuber <ansona1131@gmail.com>
2022-07-11 08:32:45 +0800

    7/9

--- TIOJ/1089.cpp
+++ TIOJ/1089.cpp
@@ -5,22 +5,33 @@ using namespace std;
 #define endl '\n'
 #define pii pair<int,int>
 
-struct Arr{
-    int dir;
-    int id;
-    int val;
-    bool operator<(Arr b) {
-        return val < b.val;
-    }
-};
-
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, k; cin>>n>>k;
     int r, c;
-    vector<vector<int> > g(n+1);
+    vector<vector<int> > gr(n+1);
+    vector<vector<int> > gc(n+1);
+    vector<int> rsz(n+1,0);
+    vector<int> csz(n+1,0);
     while(k--) {
         cin>>r>>c;
-        g[r].push_back(c);
+        gr[r].push_back(c);
+        rsz[r]++;
+        gc[c].push_back(r);
+        csz[c]++;
+    }
+    int ctrl = 0;
+    int maxi = 0, maxn = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (rsz[i] > maxn) {
+            ctrl = 0;
+            maxi = i;
+            maxn = rsz[i];
+        }
+        if (csz[i] > maxn) {
+            ctrl = 1;
+            maxi = i;
+            maxn = csz[i];
+        }
     }
 }
