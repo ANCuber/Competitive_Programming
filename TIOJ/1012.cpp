003939d24ca59e2abe2797ee7b65ae39b525f57f
ANCuber <ansona1131@gmail.com>
2022-07-12 18:42:47 +0800

    7/12

--- TIOJ/1012.cpp
+++ TIOJ/1012.cpp
@@ -13,9 +13,50 @@ int main() {
         cin>>B[i];
     }
     int id = 1;
-    vector <int> sta;
+    vector<int> s(n+5);
+    int stk = 0;
     for (int i = 1; i <= n; ++i) {
-        
+        if (B[id] == i) {
+            id++;
+            int ctrl = 1;
+            while(ctrl) {
+                int rng = min(m+1,stk);
+                if (rng < 1) break;
+                for (int j = 1; j <= rng; ++j) {
+                    if (s[stk-j] == B[id]) {
+                        for (int k = stk-j; k < stk-1; ++k) {
+                            s[k] = s[k+1];
+                        }
+                        stk--;
+                        id++;
+                        break;
+                    }
+                    if (j == rng) ctrl = 0;
+                }
+            }
+        } else {
+            int ctrl = 1;
+            while(ctrl) {
+                int rng = min(m+1,stk);
+                if (rng < 1) break;
+                for (int j = 1; j <= rng; ++j) {
+                    if (s[stk-j] == B[id]) {
+                        for (int k = stk-j; k < stk-1; ++k) {
+                            s[k] = s[k+1];
+                        }
+                        stk--;
+                        id++;
+                        break;
+                    }
+                    if (j == rng) ctrl = 0;
+                }
+            }
+            s[stk++] = i;
+        }
+        //for (int j = 0; j < stk; ++j) cout<<s[j]<<' ';
+        //cout<<"#"<<id-1<<endl;
     }
+    if (id == n+1) cout<<"yes"<<endl;
+    else cout<<"no"<<endl;
     return 0;
 }
---
+++ TIOJ/1018.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+
+void solve() {
+    string x;
+    while(cin>>x && x != "THAN") {
+        
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n; cin>>n;
+    while(n--) {
+        string x;
+        cin>>x;
+        solve();
+    }
+}
