cab5c84d086d2db3004cc47651c7156cb9af1e94
ANCuber <ansona1131@gmail.com>
2022-07-13 13:06:20 +0800

    7/12

--- TIOJ/1090.cpp
+++ TIOJ/1090.cpp
@@ -8,5 +8,9 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, st; cin>>n>>st;
-    return 0;
+    vector<int> grs(n);
+    vector<vector<ll> > dp(n,vector<ll>(n,1e18));
+    for (int i = 0; i < n; ++i) cin>>grs[i];
+    sort(grs.begin(),grs.end());
+    for (int i = 0; i < n; ++i) dp[i][i] = abs(grs[i]-st); 
 }
