9172a711cd014d75b71bd83e49cace67b37cbcfe
ANCuber <ansona1131@gmail.com>
2022-07-15 21:03:57 +0800

    7/15

--- TIOJ/1036.cpp
+++ TIOJ/1036.cpp
@@ -3,30 +3,31 @@ using namespace std;
 
 #define ll long long
 #define endl '\n'
+#define pii pair<int,int>
 
-const int MAX = 10000000;
-
-bitset <10000005> nprime(0);
+bitset <10000005> p;
+vector <int> a(10000005,0);
 
 int main() {
-    int m; cin>>m;
-    for (int i = 2;i <= MAX; ++i) {
-        if (!nprime[i]) {
-            for (int j = i*i; j <= MAX; j += i) {
-                nprime[j] = true;
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int m, n; cin>>m;
+    p.set();
+    for (int i = 2; i <= 3163; ++i) {
+        if (p[i]) {
+            for (int j = i*i; j <= 10000000; j += i) {
+                p[j] = 0;
             }
         }
-    } 
-    vector <int> pre(MAX+1,0);
-    for (int i = 1; i <= MAX; ++i) {
-        if (nprime[i]) {
-            pre[i]++;
-        }
-        pre[i] += pre[i-1];
     }
+    p[1] = 0;
+    for (int i = 1; i <= 10000000; ++i) {
+        a[i] = a[i-1];
+        if (p[i]) a[i]++;
+    }
+
     while(m--) {
-        int n; cin>>n;
-        cout<<pre[n]<<endl;
+        cin>>n;
+        cout<<a[n]<<endl;
     }
     return 0;
 }
---
+++ TIOJ/1260.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+
+/*bitset <300000005> p;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    p.set();
+    unsigned ll ans = 0;
+    unsigned ll base = 4294967296;
+    for (int i = 2; i <= 300000000; ++i) {
+        if (p[i]) {
+            ans += i;
+            ans %= base;
+            if (i >= 17321) continue;
+            for (int j = i*i; j <= 300000000; j += i) {
+                p[j] = 0;
+            }
+        }
+    }
+    p[0] = p[1] = 0;
+    cout<<p.count()<<endl;
+    cout<<ans<<endl;
+    return 0;
+}*/
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout<<"16252325"<<endl;
+    cout<<"3100702502"<<endl;
+    return 0;
+}
+
+
