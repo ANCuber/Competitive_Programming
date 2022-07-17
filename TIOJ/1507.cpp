9f06d2f610d38b0f26019f89fa1e498ab29d90a1
ANCuber <ansona1131@gmail.com>
2022-07-17 15:58:04 +0800

    7/17

---
+++ TIOJ/1507.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    string A, B;
+    char c;
+    cin>>A>>c>>B;
+}
