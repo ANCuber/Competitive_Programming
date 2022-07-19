7e14c683087fb03fd7e19519598fc77aed238c1a
ANCuber <ansona1131@gmail.com>
2022-07-19 21:54:31 +0800

    7/19

---
+++ TIOJ/1407.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pii pair<int,int>
+#define p_q priority_queue
+
+struct Rock{
+    int w, m, c;
+};
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n; cin>>n;
+    vector<Rock> r(n+1);
+    for (int i = 1; i <= n; ++i) cin>>r[i].w>>r[i].m>>r[i].c;
+    int t; cin>>t;
+    vector<vector<int> > dp(2,vector<int>(t+1,-1e9));
+    vector<int> mx(t+1,-1e9);
+    dp[0][0] = mx[0] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= t; ++j) {
+            if (j >= r[i].w) {
+                
+            } else {
+                
+            }
+        }
+    }
+     
+    return 0;
+}
