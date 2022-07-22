21797f57a3dd26eb0bbbafc5b26f2b1a6c08a117
ANCuber <ansona1131@gmail.com>
2022-07-22 12:45:45 +0800

    7/22

--- TIOJ/1415.cpp
+++ TIOJ/1415.cpp
@@ -14,11 +14,28 @@ int main() {
     while(cin>>n) {
         int k, a;
         ll ans = 0;
+        p_q <int> mx, mxrm;
+        p_q <int,vector<int>,greater<int> > mn, mnrm;
         while(n--) {
             cin>>k;
             while(k--) {
                 cin>>a;
+                mx.push(a);
+                mn.push(a);
             }
+            while(mxrm.size() && mx.top() == mxrm.top()) {
+                mx.pop();
+                mxrm.pop();
+            }
+            while(mnrm.size() && mn.top() == mnrm.top()) {
+                mn.pop();
+                mnrm.pop();
+            }
+            ans += (mx.top()-mn.top());
+            mxrm.push(mn.top());
+            mnrm.push(mx.top());
+            mx.pop();
+            mn.pop();
         }
         cout<<ans<<endl;
     }
--- TIOJ/2054.cpp
+++ TIOJ/2054.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,19 +7,29 @@ using namespace std;
 #define endl '\n'
 #define pii pair<int,int>
 #define p_q priority_queue
-#pragma GCC optimize("Ofast")
-
-struct City{
-    int x, y;
-};
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, ln, w;
     cin>>n>>ln>>w;
-    vector <City> px(n);
+    vector< pii > p(n);
+
+    for (int i = 0; i < n; ++i) cin>>p[i].first>>p[i].second;
+    sort(p.begin(),p.end);
+
+    int ci, can;
+    int ans = 0;
+    int l, r, d, u;
 
     for (int i = 0; i < n; ++i) {
+        ci = i, can = 0;
+        l = p[i].first, r = l+w;
+        d = p[i].second, u = d+ln;
+        while(p[ci].first <= r) {
+            
+        }
         
     }
+
+    return 0;
 }
