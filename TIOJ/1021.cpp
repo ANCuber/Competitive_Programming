ebdd2ef476b42ac6dcc704831f69b1edacf1d172
ANCuber <ansona1131@gmail.com>
2022-07-22 18:23:47 +0800

    7/22

--- TIOJ/1021.cpp
+++ TIOJ/1021.cpp
@@ -1,13 +1,18 @@
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
+#define ull unsigned long long
 #define endl '\n'
 #define pii pair<int,int>
+#define p_q priority_queue
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    string A, B;
-    char c;
-    cin>>A>>c>>B;
+    int n, k;
+    while(cin>>n>>k) {
+        
+    }
+    return 0;
 }
