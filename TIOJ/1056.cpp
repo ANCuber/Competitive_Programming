ade2cc18b384ebf4056128a66b7ffd861bfbce2f
ANCuber <ansona1131@gmail.com>
2022-07-22 13:25:57 +0800

    7/22

--- TIOJ/1056.cpp
+++ TIOJ/1056.cpp
@@ -4,27 +4,44 @@ using namespace std;
 #define endl '\n'
 #define ll long long
 
-int ctrl = 0;
 string ans[2] = {"POSSIBLE","IMPOSSIBLE"};
+int ctrl = 0;
+int l[3] = {0};
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    char g[3][3];
-    int cir = 0, ow = 0;
-    int cro = 0, xw = 0;
+    int a[3][3];
+    char x;
+    int O = 0, X = 0;
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            cin>>g[i][j];
-            if (g[i][j] == 'O') {
-                cir++;
-            } else if (g[i][j] == 'X') {
-                cro++;
-            } else if (g[i][j] != '.') {
-                ctrl = 1;
-            }
+            cin>>x;
+            if (x == '.') a[i][j] = 0;
+            else if (x == 'O') a[i][j] = 1, O++;
+            else if (x == 'X') a[i][j] = 2, X++;
+            else ctrl = 1;
         }
     }
-    if (cir > cro) ctrl = 1;
-    cout<<ans[ctrl]<<endl;
+    if (O-X > 1 || O-X < 0 || ctrl) {
+        cout<<ans[1]<<endl;
+        return 0;
+    }
+    for (int i = 0; i < 3; ++i) {
+        if (a[i][0] == a[i][1] && a[i][1] == a[i][2]) l[a[i][0]]++;
+        if (a[0][i] == a[1][i] && a[1][i] == a[2][i]) l[a[0][i]]++;
+    }
+    if (a[0][0] == a[1][1] && a[1][1] == a[2][2]) l[a[1][1]]++;
+    if (a[0][2] == a[1][1] && a[1][1] == a[2][0]) l[a[1][1]]++;
+
+    
+    if (l[1] > 2 || l[2] > 2) cout<<ans[1]<<endl;
+    else if (l[1] == 2 && l[2] != 0) cout<<ans[1]<<endl;
+    else if (l[2] == 2 && l[1] != 0) cout<<ans[1]<<endl;
+    else if (l[1] == 1 && l[2] == 1) cout<<ans[1]<<endl;
+    else if (l[1] == 2 && O != X+1) cout<<ans[1]<<endl;
+    else if (l[1] == 1 && O != X+1) cout<<ans[1]<<endl;
+    else if (l[2] == 1 && X != O) cout<<ans[1]<<endl;
+    else cout<<ans[0]<<endl;
+
     return 0;
 }
