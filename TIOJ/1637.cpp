ec1cad1b16ffff20247a12f4e685199e8a68b48b
ANCuber <ansona1131@gmail.com>
2022-07-27 23:11:56 +0800

    7/27

--- TIOJ/1637.cpp
+++ TIOJ/1637.cpp
@@ -5,12 +5,15 @@ using namespace std;
 #define endl '\n'
 #define pii pair<int,int>
 
+ll ans = 0;
+vector<ll> g;
+vector<ll> bit;
+
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n; cin>>n;
-    vector<ll> g(n-1);
-    for (int i = 0; i < n-1; ++i) cin>>g[i];
-    ll ans = 0;
-    
+    g.resize(n+1);
+    bit.resize(n+1);
+    for (int i = 1; i <= n; ++i) cin>>g[i];
     return 0;
 }
