8e7ffd8e0d0b8709c29b587b2f244803e27fc39a
ANCuber <ansona1131@gmail.com>
2022-08-02 09:47:34 +0800

    8/2

--- TIOJ/1085.cpp
+++ TIOJ/1085.cpp
@@ -10,23 +10,14 @@ using namespace std;
 #define pll pair<ll,ll>
 #define p_q priority_queue
 
-struct Pt{int x, y, z;};
+
+int dx[6] = {0,0,0,0,1,-1};
+int dy[6] = {1,-1,0,0,0,0};
+int dz[6] = {0,0,1,-1,0,0};
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int x, y, z;
     cin>>x>>y>>z;
-    vector<vector<vector<int> > > g(x+2,vector<vector<int> >(y+2,vector<int>(z+2,0)));
-    for (int i = 1; i <= z; ++i) {
-        for (int j = 1; j <= y; ++j) {
-            for (int k = 1; k <= x; ++k) {
-                cin>>[k][j][i];
-            }
-        }
-    }
-    queue <int> q;
-    q.push({1,1,1});
-    
-    
     return 0;
 }
--- TIOJ/1097.cpp
+++ TIOJ/1097.cpp
@@ -14,14 +14,19 @@ int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int l, w, a;
     while(cin>>l>>w && l && w) {
-        vector<vector<int> > g(l+1,vector<int>(w+1,0));
+        vector<vector<int> > dp(2,vector<int>(w+1,0));
+        int ans = 0;
         for (int i = 1; i <= l; ++i) {
             for (int j = 1; j <= w; ++j) {
                 cin>>a;
-                g[i][j] = (a != 2);
+                if (a == 2) a = 0;
+                else a = 1;
+                dp[i%2][j] = (min({dp[i%2][j-1],dp[(i+1)%2][j],dp[(i+1)%2][j-1]})+1)*a;
+                ans = max(ans,dp[i%2][j]);
+                //cout<<i<<' '<<j<<' '<<dp[i%2][j]<<endl;
             }
         }
-        
+        cout<<ans*ans<<endl;
     }
     return 0;
 }
