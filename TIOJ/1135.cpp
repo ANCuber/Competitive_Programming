ee8e3e4bde5e964be5bc0b15df7057259e0af979
ANCuber <ansona1131@gmail.com>
2022-08-03 22:36:31 +0800

    8/3

--- TIOJ/1135.cpp
+++ TIOJ/1135.cpp
@@ -11,5 +11,9 @@ typedef pair<long long,long long> pll;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    
-    }
+    int t; cin>>t;   
+    ll a, b, c, k;
+
+
+    return 0;
+}
