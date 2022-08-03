bc04c51b9398051d130e76136775a4f3a6b82ac9
ANCuber <ansona1131@gmail.com>
2022-08-03 10:14:02 +0800

    8/3

--- setting.cpp
+++ setting.cpp
@@ -2,14 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ull unsigned long long
+typedef long long ll
+typedef unsigned long long ull
+typedef pair<int,int> pii
+typedef pair<long long,long long> pll
+typedef priority_queue p_q
 #define endl '\n'
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define p_q priority_queue
+
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
+    
 }
