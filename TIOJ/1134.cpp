7b16f28bd686b56efc150a17ff419b751ac0d776
ANCuber <ansona1131@gmail.com>
2022-08-08 11:59:31 +0800

    8/8

--- TIOJ/1134.cpp
+++ TIOJ/1134.cpp
@@ -15,13 +15,33 @@ int main() {
     int n, m;
     while(t--) {
         cin>>n>>m;
-        vector<vector<int> > g(205,vector<int>(205));
+        vector<vector<int> > g(205,vector<int>(205,0));
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= m; ++j) {
                 cin>>g[i][j];
+                g[i][j] = 1-g[i][j];
+                g[i][j] += g[i-1][j];
             }
         }
-        
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i+1; j <= n; ++j) {
+                int lp = 0 , cnt = 0;
+                for (int k = 1; k <= m; ++k) {
+                    cnt += g[j][k]-g[i][k];
+                    while (lp < k && cnt <= (k-lp)*(j-i)-cnt) {
+                        cnt -= g[j][++lp]-g[i][lp];
+                    }
+                    if (cnt > (k-lp)*(j-i)-cnt) ans = max(ans,(k-lp)*(j-i));
+                }
+            }
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
+
+
+
+
+
