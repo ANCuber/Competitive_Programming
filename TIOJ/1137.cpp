022ac88eb437b418335573f8b86d7335d646e739
ANCuber <ansona1131@gmail.com>
2022-08-26 17:01:17 +0800

    8/26

--- TIOJ/1137.cpp
+++ TIOJ/1137.cpp
@@ -9,18 +9,25 @@ typedef pair<long long,long long> pll;
 #define p_q priority_queue
 #define endl '\n'
 
-vector<int> v, num, low;
-int cnt;
+vector<int> num, low, prt;
+bitset<10005> ans;
+int cnt, n, m;
 
-void dfs(int cur, int fa, vector<vector<int> > &g) {
+void dfs(int cur, vector<vector<int> > &g) {
     num[cur] = low[cur] = ++cnt;
     int child = 0;
     for (auto i : g[cur]) {
         if (!num[i]) {
-            chlid++;
-            dfs(i,cur,g);
-        } else if(i != fa){
-            
+            child++;
+            prt[i] = cur;
+            dfs(i,g);
+            low[cur] = min(low[i],low[cur]);
+
+            if ((prt[cur] == -1 && child > 1) || (prt[cur] != -1 && low[i] >= num[cur])) {
+                ans[cur] = 1;
+            }
+        } else if(i != prt[cur]){
+            low[cur] = min(low[cur],num[i]);
         }
     }
 }
@@ -29,22 +36,27 @@ int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int t; cin>>t;    
     while(t--) {
-        v.clear(); cnt=0;
+        ans.reset(); cnt=0;
         cin>>n>>m;
         vector<vector<int> > g(n+1);
-        num.assign(n+1,0), low.assign(n+1,1e9);
+        num.assign(n+1,0), low.assign(n+1,1e9), prt.assign(n+1,-1);
         int a, b;
         for (int i = 0; i < m; ++i) {
             cin>>a>>b;
             g[a].push_back(b);
             g[b].push_back(a);
         }
-        dfs(1,0,g);
-        sort(v.begin(),v.end());
-        cout<<v.size()<<endl;
-        for (int i = 0; i < v.size(); ++i) {
-            if (i) cout<<' ';
-            cout<<v[i];
+        dfs(1,g);
+        cout<<ans.count()<<endl;
+        if(!ans.count()) {cout<<0<<endl; continue;}
+        int flag = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (ans[i] && !flag) {
+                cout<<i;
+                flag = 1;
+            } else if (ans[i]) {
+                cout<<' '<<i;
+            }
         }
         cout<<endl;
     }
