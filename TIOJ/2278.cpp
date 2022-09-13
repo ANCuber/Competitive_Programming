9da560ceec1032780ab5f1a1562281db2ad87130
ANCuber <ansona1131@gmail.com>
2022-09-13 22:14:15 +0800

    9/13

--- TIOJ/2278.cpp
+++ TIOJ/2278.cpp
@@ -8,15 +8,22 @@ typedef pair<int,int> pii;
 typedef pair<long long,long long> pll;
 #define p_q priority_queue
 #define endl '\n'
+#define int ll
 
-int main() {
+const int mod = 1e9+7;
+
+signed main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, a, sum; cin>>n>>a;
     vector<int> dp(n+1,0);
     dp[a] = sum = 1;
-    for (int i = 2; i <= n; ++i) {
+    for (int j = 2; j <= n; ++j) {
         cin>>a;
-        
+        int i = sum-dp[a];
+        i = (i+mod)%mod;
+        dp[a] += i; dp[a] %= mod;
+        sum += i; sum %= mod;
     }
+    cout<<sum<<endl;
     return 0;
 }
