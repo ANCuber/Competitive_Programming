c7c900bf550f4bf76da83aa3b00b903826a977c7
ANCuber <ansona1131@gmail.com>
2022-09-24 23:11:45 +0800

    9/24

---
+++ TIOJ/1318.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int,int>
+#define pll pair<long long,long long> 
+#define p_q priority_queue
+#define endl '\n'
+
+bool cmp(pii a, pii b) {
+    return a.second<b.second;
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, t, ans = 0;
+    cin>>n>>t;
+    
+    return 0;
+}
