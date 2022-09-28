e2ce92ee062206648f60e87e6ce2d6f034fa8b88
ANCuber <ansona1131@gmail.com>
2022-09-28 10:57:08 +0800

    9/28

--- TIOJ/1318.cpp
+++ TIOJ/1318.cpp
@@ -41,23 +41,30 @@ signed main() {
     for (int i = 1; i < lw.size(); ++i) lw[i] += lw[i-1];
     for (int i = 1; i < hg.size(); ++i) s[i] = s[i-1]+hg[i].c;
 
-    vector<int> mn(hg.size());
-    mn[hg.size()-1] = hg[hg.size()-1].h;
+    vector<int> mn(hg.size(),0), mx(hg.size(),0);
+    mn[hg.size()-1] = hg[hg.size()-1].h; mx[1] = hg[1].c-hg[1].h;
     for (int i = hg.size()-2; i > 0; --i) mn[i] = min(mn[i+1],hg[i].h);
+    for (int i = 2; i < hg.size(); ++i) mx[i] = max(mx[i-1],hg[i].c-hg[i].h);
 
     int ans = 0;
     
     for (int i = 0; i < hg.size(); ++i) {
         int cur = t-s[i];
+        if (cur < 0) break;
         ans = max(ans,i*2+(upper_bound(lw.begin(),lw.end(),cur)-lw.begin()));
+    }
+    for (int i = 1; i < hg.size(); ++i) {
+        int cur = t-s[i];
+        cur += mx[i];
+        if (cur < 0) break;
+        ans = max(ans,i*2-1+(upper_bound(lw.begin(),lw.end(),cur)-lw.begin()));
     }
     for (int i = 0; i < hg.size()-1; ++i) {
         int cur = t-s[i];
         cur -= mn[i+1];
+        if (cur < 0) break;
         ans = max(ans,i*2+1+(upper_bound(lw.begin(),lw.end(),cur)-lw.begin()));
     }
     cout<<ans<<endl;
-    
-    
     return 0;
 }
