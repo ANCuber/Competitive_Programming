f11ba122b04239f78c51d078fa3635ab6f7e36db
ANCuber <ansona1131@gmail.com>
2022-09-28 19:26:16 +0800

    9/28

--- setting.cpp
+++ setting.cpp
@@ -8,8 +8,9 @@ using namespace std;
 #define pll pair<long long,long long> 
 #define p_q priority_queue
 #define endl '\n'
+#define pb push_back
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    
+        
 }
