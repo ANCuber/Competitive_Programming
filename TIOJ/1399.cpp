17600ca89eca2a15b5298d7d16791828c67cbc70
ANCuber <ansona1131@gmail.com>
2022-10-02 22:45:37 +0800

    10/2

--- TIOJ/1399.cpp
+++ TIOJ/1399.cpp
@@ -11,28 +11,44 @@ using namespace std;
 #define pb push_back
 #define int long long
 
-struct my{
-    int first, second, flag;
-    bool operator<(my b) {
-        if (first == b.first) return second<b.second;
-        return first<b.first;
-    }
-};
-
 signed main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int t, n;
     while(cin>>t>>n) {
-        p_q <int> pq;
         vector<int> f(n), d(n);
+        p_q <int,vector<int>, greater<int> > pq;
         for (int i = 0; i < n; ++i) cin>>f[i];
         for (int i = 0; i < n; ++i) cin>>d[i];
         
-        int ans = 0;
+        int ans = 0, cur = 0, flag = 0;
         for (int i = 0; i < n; ++i) {
             int a; cin>>a;
+            if (flag) continue;
             t -= a;
-            
+            while(t < 0 && !pq.empty()) {
+                cur -= pq.top();
+                pq.pop();
+                t++;
+            }
+            if (t < 0 || (!t && pq.empty())) {
+                flag = 1;
+                continue;
+            }
+            for (int j = 0; f[i]-j*d[i] > 0; ++j) {
+                if (t > 0) {
+                    pq.push(f[i]-j*d[i]);
+                    cur += f[i]-j*d[i];
+                    t--;
+                } else if (t == 0 && f[i]-j*d[i] > pq.top()) {
+                    cur -= pq.top();
+                    pq.pop();
+                    pq.push(f[i]-j*d[i]);
+                    cur += (f[i]-j*d[i]);
+                } else {
+                    break;
+                }
+            }
+            ans = max(ans,cur);
         }
         cout<<ans<<endl;
     }
