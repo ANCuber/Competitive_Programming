c7793d3cab3e089d1d376dd9a4400fff49767f0b
ANCuber <ansona1131@gmail.com>
2022-10-05 08:32:11 +0800

    10/5

--- cses/ide.cpp
+++ cses/ide.cpp
@@ -1,57 +1,25 @@
-#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
-#define ull unsigned long long
 #define pii pair<int,int>
-#define pll pair<long long,long long> 
 #define p_q priority_queue
 #define endl '\n'
-#define int long long
-
-const int mod = 1e9+7;
+#define pb push_back
 
 signed main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, m; cin>>n>>m;
-    vector<int> a(n);
-    for (auto& i : a) cin>>i;
-    vector<int> dp(m+2,0);
-    if (a[0]) {
-        dp[a[0]]++;
-    } else {
-        for (auto& i : dp) i = 1;
-        dp[0] = dp[m+1] = 0;
+    int n, q; cin>>n>>q;    
+    vector<int> arr(n+1,0);
+    for (int i = 1; i <= n; ++i) {
+        cin>>arr[i];
+        arr[i] ^= arr[i-1];
+    }
+    while(q--) {
+        int l, r; cin>>l>>r;
+        cout<<(arr[l-1]^arr[r])<<endl;
     }
     
-    for (int i = 1; i < n; ++i) {
-        if (a[i]) {
-            for (int j = 1; j <= m; ++j) {
-                if(abs(a[i]-j) == 1) dp[a[i]] = (dp[j]+dp[a[i]])%mod;  
-                if(j != a[i]) dp[j] = 0;
-            }
-        } else {
-            vector<int> tmp = dp;
-            for (int j = 1; j <= m; ++j) {
-                dp[j] = tmp[j-1]+tmp[j]+tmp[j+1];
-                dp[j] %= mod;
-            }
-        }
-        //for (auto i : dp) cout<<i;
-        //cout<<endl;
-    }
-
-    if (a[n-1]) {
-        cout<<dp[a[n-1]]<<endl;
-    } else {
-        int ans = 0;
-        for (int i = 1; i <= m; ++i) {
-            ans += dp[i];
-            ans %= mod;
-        }
-        cout<<ans<<endl;
-    }
-
     return 0;
 }
