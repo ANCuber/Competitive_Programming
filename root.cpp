b367d9e39addded091c88040ff0e9b8b0fa79f47
ANCuber <ansona1131@gmail.com>
2022-10-11 22:48:57 +0800

    10/11

--- root.cpp
+++ root.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double ROOT(double,int);
-double mypow(double,int);
-const double eps = 1e-12;
+long double ROOT(long double,int);
+long double mypow(long double,int);
+const long double eps = 1e-13;
 
 int main() { 
-    double x; int y;
+    long double x; int y;
     cout<<"請輸入一個數： ";
     cin>>x;
     cout<<"請輸入一正整數來開幾次方： ";
@@ -14,14 +14,14 @@ int main() {
     if(x<0 &&y%2==0 || y<=0 ) {
         printf("無定義\n");
     } else {
-        cout<<x<<"的開"<<y<<"次方是:"<<endl;
-        cout<<fixed<<setprecision(10)<<ROOT(x,y)<<endl;
+        cout<<"ANS: ";
+        cout<<fixed<<setprecision(12)<<ROOT(x,y)<<endl;
     }
     return 0;
 }
 
-double ROOT(double x,int y) {
-    double left,right, mid;
+long double ROOT(long double x,int y) {
+    long double left,right, mid;
     if(x>=1) right=x,left=1;
     else right=1,left=x;
     
@@ -38,8 +38,8 @@ double ROOT(double x,int y) {
     return mid;
 }
 
-double mypow(double x,int n) {
-    double k = x;
+long double mypow(long double x,int n) {
+    long double k = x;
     for(int i=1;i<n;i++) x = x*k;
     return x;
 }
