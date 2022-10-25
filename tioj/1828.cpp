173b89b435cfeeed5f1e3aec127d0a6772cf7e15
ANCuber <ansona1131@gmail.com>
2022-10-25 11:59:59 +0800

    10/24

---
+++ tioj/1828.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define p_q priority_queue
+#define endl '\n'
+#define pb push_back
+
+const unsigned  int mxd = 1e4;
+unsigned int x, y, z;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t; cin>>t;    
+    while(t--) {
+        cin>>x>>y>>z;
+        if (x > mxd) cout<<0<<endl;
+        else {
+            
+        }
+    }
+    
+    return 0;
+}
