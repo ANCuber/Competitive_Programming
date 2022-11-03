305f42870a7668a598a42b2a01743e6e167aacdd
ANCuber <ansona1131@gmail.com>
2022-11-03 13:18:30 +0800

    11/3

--- tioj/2147.cpp
+++ tioj/2147.cpp
@@ -3,24 +3,29 @@
 using namespace std;
 
 #define ll long long
-#define pii pair<int,int>
-#define p_q priority_queue
-#define endl '\n'
-#define pb push_back
 
-int n, k, a;
-int dp[201] = {0};
-int mx[201] = {0};
-ll ans = 0;
+int n, k;
+ll ans = 2e18;
 
 signed main() {
-    scanf("%d%d",&n,&k);    
+    scanf("%d%d",&n,&k);
+    vector<vector<ll> > dp(n+1,vector<ll>(k+1,2e18));
+    vector<ll> mn(k+1,0), a(n+1,0);
     for (int i = 1; i <= n; ++i) {
-        scanf("%d",&a);
-        for (int i = 1; i <= k; ++i) {
-            
+        dp[i][1] = 0;
+    }
+    for (int i = 1; i <= n; ++i) {
+        scanf("%lld",&a[i]);
+        for (int j = 2; j <= k; ++j) {
+            for (int l = 1; l <= i-1; ++l) {
+                ll cur = dp[l][j-1]+max(a[l]-a[i],0LL);
+                if (dp[i][j] > cur || (dp[i][j] == cur && a[i]-a[l] )) {
+                    a[i] = a[l];
+                }
+            }
         }
     }
+    for (int i = 1; i <= n; ++i) ans = min(ans,dp[i][k]);
     printf("%lld\n",ans);
     return 0;
 }
