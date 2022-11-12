d6e9ca02b1e7a40d5ad7600a31409705cdc1ab01
ANCuber <ansona1131@gmail.com>
2022-11-12 09:48:05 +0800

    11/12

---
+++ cses/1645.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define p_q priority_queue
+#define endl '\n'
+#define pb push_back
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n; cin>>n;    
+    
+    
+    return 0;
+}
