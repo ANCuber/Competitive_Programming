1cb24d003cb11f78ea8847426e20e3d19892cbc3
ANCuber <ansona1131@gmail.com>
2022-12-13 20:38:29 +0800

    12/13

---
+++ cses/1137.cpp
@@ -0,0 +1,75 @@
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define p_q priority_queue
+#define endl '\n'
+#define pb push_back
+
+#define int ll
+
+struct BIT{
+    vector<int> a;
+    int sz;
+    void init(int _n) {
+        sz = _n;
+        a.assign(_n+5,0);
+    }
+    void upd(int p, int v) {
+        while(p <= sz) {
+            a[p] += v;
+            p += p&-p;
+        }
+    }
+    int sum(int p) {
+        int ret = 0;
+        while(p > 0) {
+            ret += a[p];
+            p -= p&-p;
+        }
+        return ret;
+    }
+} bit;
+
+int tim = 0;
+
+void dfs(int cur, int fa, vector<int> &st, vector<int> &ed, vector<vector<int> > &g) {
+    st[cur] = ++tim;
+    for (auto v : g[cur]) {
+        if (v != fa) dfs(v,cur,st,ed,g);
+    }
+    ed[cur] = tim;
+}
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, q; cin>>n>>q;    
+    vector<int> v(n+1,0);
+    for (int i = 1; i <= n; ++i) cin>>v[i];
+    int a, b;
+    vector<vector<int> > g(n+1);
+    for (int i = 1; i < n; ++i) {
+        cin>>a>>b;
+        g[a].pb(b);
+        g[b].pb(a);
+    }
+    vector<int> st(n+1,0), ed(n+1,0);
+    dfs(1,0,st,ed,g);
+    bit.init(n);
+    for (int i = 1; i <= n; ++i) bit.upd(st[i],v[i]);
+    
+    int cmd, s, x;
+    while(q--) {
+        cin>>cmd>>s;
+        if (cmd == 1) {
+            cin>>x;
+            bit.upd(st[s],x-v[s]);
+            v[s] = x;
+        } else {
+            cout<<(bit.sum(ed[s])-bit.sum(st[s]-1))<<endl;
+        }
+    }
+    return 0;
+}
