2f9c0519ec84bc7811c83b545242079d62193a14
ANCuber <ansona1131@gmail.com>
2023-03-03 15:45:10 +0800

    3/3

--- cses/1641.cpp
+++ cses/1641.cpp
@@ -1,19 +1,19 @@
-#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
-
-#define int long long
+ 
 #define pii pair<int,int>
 #define p_q priority_queue
 #define endl '\n'
 #define pb push_back
-
+ 
+int a[5005];
+int n, x;
+ 
 signed main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, x; 
-    cin>>n>>x;    
+    cin>>n>>x;
     unordered_map< int,pii > mp;
-    vector<int> a(n+1);
     for (int i = 1; i <= n; ++i) cin>>a[i];
     for (int i = 1; i <= n; ++i) {
         if (mp.count(x-a[i])) {
@@ -21,7 +21,7 @@ signed main() {
             return 0;
         }
         for (int j = 1; j < i; ++j) {
-            mp[a[i]+a[j]] = {j,i};
+            if (a[i]+a[j] < x) mp[a[i]+a[j]] = {j,i};
         }
     }
     cout<<"IMPOSSIBLE"<<endl;
