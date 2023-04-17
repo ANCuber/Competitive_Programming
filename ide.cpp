8d89e2b703504202b61dc52782e8ba190fa24fdf
Anson <ansona1131@gmail.com>
2023-04-17 21:39:41 +0800

    4/17

--- ide.cpp
+++ ide.cpp
@@ -7,51 +7,20 @@ using namespace std;
 #define p_q priority_queue
 #define endl '\n'
 #define pb push_back
-#define mid ((l+r)>>1)
 
-struct SEG{
-    vector<int> a, tg;
-    void init(int _n) {
-        a.assign(_n<<2,0);
-        tg.assign(_n<<2,0); 
-    } 
-    void push(int p, int c) {
-        if (tg[p]) {
-            a[p] += tg[p];
-            if (c) {
-                tg[p<<1] += tg[p];
-                tg[p<<1|1] += tg[p];
-            }
-            tg[p] = 0;
-        }
-    }
-    void add(int l, int r, int p, int ql, int qr, int v) {
-        push(p,r-l);
-        if (ql <= l && r <= qr) {
-            tg[p] += v;
-            return;
-        }
-        if (qr <= mid) add(l,mid,p<<1,ql,qr,v);
-        else if (ql > mid) add(mid+1,r,p<<1|1,ql,qr,v);
-        else {
-            add(l,mid,p<<1,ql,qr,v);
-            add(mid+1,r,p<<1|1,ql,qr,v);
-        }
-        a[p] = max(a[p<<1]+tg[p<<1],a[p<<1|1]+tg[p<<1|1]);
-    }
-    int query(int l, int r, int p, int ql, int qr) {
-        push(p,r-l);
-        if (ql <= l && r <= qr) return a[p];
-        if (qr <= mid) return query(l,mid,p<<1,ql,qr);
-        else if (ql > mid) return query(mid+1,r,p<<1|1,ql,qr);
-        else return max(query(l,mid,p<<1,ql,qr),query(mid+1,r,p<<1|1,ql,qr));
-    }
-} seg;
-
+int power(int x, int n) {
+     int ans = 1, base = x;
+     while(n) {
+         if (n&1) ans *= base;
+         base *= base;
+         n >>= 1;
+     }
+     return ans;
+}
 
 signed main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-        
-    
+    int a, b; cin>>a>>b;
+    cout<<power(a,b)<<endl;
     return 0;
 }
