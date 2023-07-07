4c3ef306c4c99ea3a13419fe348cf20002bfcf3f
ancuber1031 <ansonlai@pop-os.localdomain>
2023-07-07 23:41:49 +0800

    7/7

--- codeforces/1843e.cpp
+++ codeforces/1843e.cpp
@@ -8,13 +8,37 @@ using namespace std;
 #define endl '\n'
 #define pb push_back
 
-void Run() {
-    int n, m; cin>>n>>m;
-    while(m--) {
-        
+int n, m, q;
+
+bool check(int g, vector<int> &a, vector<pii> &M) {
+    vector<int> v(n+5,0);
+    for (int i = 1; i <= n; ++i) {
+        v[i] = v[i-1]+(a[i] <= g);
     }
-    int q; cin>>q;
-    
+    for (auto [l,r] : M) {
+        if (v[r]-v[l-1] > (r-l+1)/2) return 1;
+    }
+    return 0;
+}
+
+void Run() {
+    cin>>n>>m;
+    vector<pii> M(m);
+    vector<int> a(n+5,1e9);
+    for (auto& [l,r] : M) cin>>l>>r;
+    cin>>q;
+    for (int i = 1; i <= q; ++i) {
+        int c; cin>>c;
+        a[c] = i;
+    }
+    int l = 0, r = q+1;
+    while(r-l > 1) {
+        int mid = (l+r)>>1;
+        if (check(mid,a,M)) r = mid;
+        else l = mid;
+    }
+    if (r == q+1) cout<<-1<<endl;
+    else cout<<r<<endl;
 }
 
 signed main() {
