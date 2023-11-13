824d125436f4608db31382232bcbb58cb8a79a6e
ancuber1031 <ancuber1031@pop-os.localdomain>
2023-11-13 23:00:34 +0800

    11/13

---
+++ codeforces/1895D.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize("Ofast","unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define p_q priority_queue
+#define endl '\n'
+#define pb push_back
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n; cin>>n;    
+    vector<int> a(n+2);
+    for (int i = 1; i <= n; ++i) cin>>a[i];
+    
+    
+    return 0;
+}
--- codeforces/226B.cpp
+++ codeforces/226B.cpp
@@ -3,34 +3,32 @@
 using namespace std;
 
 #define int long long
-#define pii pair<int,int>
-#define p_q priority_queue
 #define endl '\n'
-#define pb push_back
 
 signed main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n; cin>>n;
-    vector<int> a(n+1,0), ans(100005,0);
+    vector<int> a(n+1,0);
     for (int i = 1; i <= n; ++i) cin>>a[i];
-    sort(a.begin(),a.end());
+    sort(a.begin()+1,a.end());
+    int A = 0;
+    for (int i = 1; i <= n; ++i) A += a[i]*(n-i);
     for (int i = 1; i <= n; ++i) a[i] += a[i-1];
     int q; cin>>q;
     for (int i = 0; i < q; ++i) {
         if (i) cout<<' ';
         int k; cin>>k;
-        int L = max(1ll,n-k), R = n-1, cnt = 1;
+        if (k == 1) {
+            cout<<A;
+            continue;
+        }
         int ans = 0;
-        do {
-            ans += (a[R]-a[L-1])*cnt;
-            cnt++;
-            k *= k;
-            R = L-1;
-            L = max(1ll,R-k+1);
-        } while(R >= L);
+        for (int R = n, c = 1, m = 0; R > 0; m++, R -= c, c *= k) {
+            ans += (a[R]-a[max(0ll,R-c)])*m;
+            //cout<<R<<' '<<R-c<<endl;
+        }
         cout<<ans;
     }
     cout<<endl;
     return 0;
 }
-
