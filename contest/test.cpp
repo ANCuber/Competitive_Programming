4d9188675df6d83445d041811635c0d5c62efbe3
LAI <ancuber1031@LAIs-Laptop.local>
2024-06-05 18:54:44 +0800

    try gcc on mac

--- contest/test.cpp
+++ contest/test.cpp
@@ -1,16 +1,9 @@
 #pragma GCC optimize("Ofast","unroll-loops")
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-#define ll long long
-#define pii pair<int,int>
-#define p_q priority_queue
-#define endl '\n'
-#define pb push_back
 
 signed main() {
     cin.tie(0)->sync_with_stdio(0);
     cout<< "Hello World!" << endl;
-    
     return 0;
 }
