474bb013efffb93ea0120073a9d5ff836734c343
ancuber1031 <ansona1131@gmail.com>
2024-09-18 13:05:43 +0800

    rearraging 2024/09/18

--- contest/test.cpp
+++
@@ -1,9 +0,0 @@
-#pragma GCC optimize("Ofast","unroll-loops")
-#include <iostream>
-using namespace std;
-
-signed main() {
-    cin.tie(0)->sync_with_stdio(0);
-    cout<< "Hello World!" << endl;
-    return 0;
-}
