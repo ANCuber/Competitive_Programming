1788d815ca75d7dd14cd99b717703e4497b292c4
ancuber1031 <ansona1131@gmail.com>
2024-09-27 19:43:30 +0800

    9/27

--- test.cpp
+++ test.cpp
@@ -1,17 +1,26 @@
 #pragma GCC optimize("Ofast","unroll-loops")
 #include <bits/stdc++.h>
-#include "mpi.h"
 using namespace std;
 
-#define ll long long
+#define int long long
 #define pb push_back
 
 signed main() {
-    cin.tie(0)->sync_with_stdio(0);
-    concurrency::parallel_for(0,1e8, [&](){
-        int cnt = 0;
-        for (int i = 1; i < 2e6; ++i) cnt++;
-        cout<<cnt<<' ';
-    });
+    int A, B;
+    scanf("%lld%lld", &A, &B);
+    if (A < B) swap(A, B);
+    int a = A, b = B;
+    int HCF, LCM, num = -1;
+    while(num != 0) {
+        num = a%b;
+        a = b;
+        b = num;
+
+        if (num == 0) {
+            HCF = a;
+            LCM = A*B/HCF;
+        }
+    }
+    printf("%d\n", HCF);
     return 0;
 }
---
+++ zerojudge/test.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize("Ofast","unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int a, b; cin>>a>>b;
+    cout<<__gcd(a,b)<<endl;
+    return 0;
+}
