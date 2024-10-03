cf7dc6d59be11be80b8cc748243e6a8a8de9a3b6
ancuber1031 <ansona1131@gmail.com>
2024-10-03 20:43:08 +0800

    flood hw update 10/3

--- CP_EE/Flood_simulation.cpp
+++ CP_EE/Flood_simulation.cpp
@@ -2,9 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define pb push_back
-
 float Elevation[50][50] = {
 {0,0,0,0,1,1,1,1,1,2,2,2,2,2,2,3,3,4,4,3,3,3,4,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1},
 {0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,3,3,4,4,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1},
@@ -68,8 +65,8 @@ signed main() {
     vector<vector<double> > wtr(55, vector<double>(55,0)), dw(55,vector<double>(55,0)), ele(55,vector<double>(55,2e9));
     for (int i = 1; i <= 50; ++i) for (int j = 1; j <= 50; ++j) ele[i][j] = Elevation[i-1][j-1];
     ele[0][0] = ele[0][1] = ele[0][2] = ele[0][3] = ele[0][4] = ele[1][0] = ele[2][0] = ele[3][0] = ele[4][0] = 0;
-    int hrs = 0, flg = 1;
-    while(true) {
+    int hrs = 0, flg = 0;
+    while(!flg) {
         if (++hrs <= prd) {
             for (int i = 1; i <= 50; ++i) {
                 for (int j = 1; j <= 50; ++j) {
@@ -77,12 +74,10 @@ signed main() {
                 }
             }
         }
-        for (int i = 0; i <= 51; ++i) for (int j = 0; j <= 51; ++j) dw[i][j] = 0;
         for (int i = 1; i <= 50; ++i) {
             for (int j = 1; j <= 50; ++j) {
                 for (int k = 0; k < 4; ++k) {
                     double cur = max(0.0,min(wtr[i][j]+ele[i][j]-wtr[i+dx[k]][j+dy[k]]-ele[i+dx[k]][j+dy[k]],wtr[i][j]))/4.0;
-                    //double cur = min(max(wtr[i][j]+ele[i][j]-wtr[i+dx[k]][j+dy[k]]-ele[i+dx[k]][j+dy[k]],0.0),wtr[i][j])/4.0;
                     dw[i][j] -= cur;
                     dw[i+dx[k]][j+dy[k]] += cur;
                 }
@@ -92,10 +87,10 @@ signed main() {
         for (int i = 1; i <= 50; ++i) {
             for (int j = 1; j <= 50; ++j) {
                 wtr[i][j] += dw[i][j];
+                dw[i][j] = 0;
                 if (Elevation[i-1][j-1] && wtr[i][j] >= 0.1) flg = 0;
             }
         }
-        if (flg) break;
     }
     cout<<"It takes "<<hrs<<" hour(s) until all water level (excluding river) is less than 1cm higher than the elevation."<<endl;
     return 0;
