a27bdca955515b2e2b882005dc7ec04c4d58df1c
ancuber1031 <ansona1131@gmail.com>
2024-10-07 23:27:57 +0800

    trying nvim 10/7

--- test.cpp
+++ test.cpp
@@ -5,26 +5,9 @@ using namespace std;
 #define ll long long
 #define pb push_back
 
-void add1(int x, int y) {
-    x += 1;
-    y += 1;
-}
-
-void add2(int &x, int &y) {
-    x += 1;
-    y += 1;
-}
-
-void add3(int x[3]) {
-    for (int i = 0; i < 3; ++i) x[i]++;
-}
-
 signed main() {
     cin.tie(0)->sync_with_stdio(0);
     int a, b; cin>>a>>b;
-    int c[3] = {1,2,3};
-    add1(a,b); cout<<a<<' '<<b<<endl;
-    add2(a,b); cout<<a<<' '<<b<<endl;
-    add3(c); cout<<c[0]<<' '<<c[1]<<' '<<c[2]<<endl;
+    cout<<a+b<<endl;
     return 0;
 }
