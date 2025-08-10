69c1d7a5f466a0bd65b749d456a19d5c3190611a
ancuber1031 <ansona1131@gmail.com>
2025-08-11 00:45:39 +0800

    CSES 8/11

--- cses/3403.cpp
+++ cses/3403.cpp
@@ -11,16 +11,15 @@ signed main() {
     vector<int> a(n + 5), b(m + 5);
     for (int i = 1; i <= n; ++i) cin >> a[i];
     for (int i = 1; i <= m; ++i) cin >> b[i];
-    vector<vector<int> > dp(2, vector<int>(m + 5, 0));
+    vector<vector<int> > dp(n + 5, vector<int>(m + 5, 0)), prv(n + 5, vector<int>(m + 5, 0));
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
-            if (a[i] == b[j]) dp[i & 1][j] = dp[i & 1 ^ 1][j - 1] + 1;
-            else dp[i & 1][j] = dp[i & 1 ^ 1][j - 1];
-            dp[i & 1][j] = max({dp[i & 1][j], dp[i & 1 ^ 1][j], dp[i & 1][j - 1]});
-            
+            if (a[i] == b[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = dp[i - 1][j - 1];
+            dp[i][j] = max({dp[i][j], dp[i - 1][j], dp[i][j - 1]});
         }
     }
-    cout << dp[n & 1][m] << "\n";
+    cout << dp[n][m] << "\n";
     vector<int> ans;
     return 0;
 }
